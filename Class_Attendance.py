c2443720b7ef584100f674d02b70fb6681d4c3c5
KaninWithRice <mishalnorona014@gmail.com>
2023-04-28 01:38:15 +0800

    Psuedocode

---
+++ Class_Attendance.py
@@ -0,0 +1,7 @@
+# Mohammad Mishal S. Noroña | BSCPE 1-5 | Porblem 1 Multiple Line of Text Into a Text File
+# Add introduction
+# Ask for User's Input
+# Ask if he/she wants to input another
+# If y the program will ask for another name
+# If n the program will end
+# Prints Attendance List

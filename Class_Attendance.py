e22652ef252f905d8b813fb71bc5327b1e438561
KaninWithRice <mishalnorona014@gmail.com>
2023-04-28 01:41:47 +0800

    Introduction

--- Class_Attendance.py
+++ Class_Attendance.py
@@ -1,5 +1,8 @@
 # Mohammad Mishal S. Noroña | BSCPE 1-5 | Porblem 1 Multiple Line of Text Into a Text File
 # Add introduction
+print("")
+print("WELCOME TO THE ATTENDANCE LIST ".center(40," ") )
+print("By: Mishal Noroña".center(40," ") )
 # Ask for User's Input
 # Ask if he/she wants to input another
 # If y the program will ask for another name
